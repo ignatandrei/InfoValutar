ef1cc03340a69270c9e8246ba93db7d3e87b5896
ignatandrei <ignatandrei@yahoo.com>
2019-10-28 21:08:34 +0200

    reading NBR from internet

---
+++ InfoValutar/InfoValutarDOS/ExchangeRates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoValutarDOS
+{
+    class ExchangeRates
+    {
+        public DateTime date { get; set; }
+        public string ExchangeFrom { get; set; }
+        public string ExchangeTo { get; set; }
+        public decimal ExchangeValue { get; set; }
+    }
+}

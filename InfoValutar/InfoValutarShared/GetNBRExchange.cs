d1e3eda3ca27f2614e6965500bf3e7ed28902f21
ignatandrei <ignatandrei@yahoo.com>
2019-10-31 08:09:02 +0200

    mocking request

--- InfoValutar/InfoValutarShared/GetNBRExchange.cs
+++ InfoValutar/InfoValutarShared/GetNBRExchange.cs
@@ -11,36 +11,45 @@ namespace InfoValutarShared
 {
     public class GetNBRExchange
     {
-        private static readonly HttpClient httpClient = new HttpClient();
+        private readonly HttpClient httpClient;
 
+        public GetNBRExchange(HttpMessageHandler handler = null)
+        {
+            if (handler != null)
+                httpClient = new HttpClient(handler, disposeHandler: false);
+            else
+                httpClient = new HttpClient();
+        }
         public async IAsyncEnumerable<ExchangeRates> GetActualRates()
         {
             var xml = await httpClient.GetStringAsync("https://www.bnr.ro/nbrfxrates.xml");
             //Console.WriteLine($"{xml}");
             var serializer = new XmlSerializer(typeof(DataSet));
+            DataSet result;
             using (var reader = new StringReader(xml))
             {
-                var result = serializer.Deserialize(reader) as DataSet;
-                var val = result.Body.Cube.First();
-                var date = val.date;
-                string orig = "BNR";
-                foreach (var item in val.Rate)
-                {
-                    var exch = new ExchangeRates();
-                    exch.Bank = orig;
-                    exch.date = date;
-                    exch.ExchangeTo = "RON";
-                    exch.ExchangeFrom = item.currency;
-                    exch.ExchangeValue = item.Value;
-                    if (!string.IsNullOrWhiteSpace(item.multiplier))
-                    {
-                        exch.ExchangeValue = exch.ExchangeValue / (decimal)int.Parse(item.multiplier);
-                    }
-                    yield return exch;
-                }
-
-
+                result = serializer.Deserialize(reader) as DataSet;
             }
+            var val = result.Body.Cube.First();
+            var date = val.date;
+            string orig = "BNR";
+            foreach (var item in val.Rate)
+            {
+                var exch = new ExchangeRates();
+                exch.Bank = orig;
+                exch.date = date;
+                exch.ExchangeTo = "RON";
+                exch.ExchangeFrom = item.currency;
+                exch.ExchangeValue = item.Value;
+                if (!string.IsNullOrWhiteSpace(item.multiplier))
+                {
+                    exch.ExchangeValue = exch.ExchangeValue / (decimal)int.Parse(item.multiplier);
+                }
+                yield return exch;
+            }
+
+
         }
     }
 }
+
--- InfoValutar/InfovalutarTest/TestNBR.cs
+++ InfoValutar/InfovalutarTest/TestNBR.cs
@@ -1,5 +1,7 @@
 using InfoValutarShared;
+using RichardSzalay.MockHttp;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,6 +18,25 @@ namespace InfovalutarTest
                 if(e.ExchangeFrom == "EUR")
                 {
                     Assert.True(true);
+                    return;
+                }
+            }
+            Assert.True(false, "Should find EUR");
+        }
+        [Fact]
+        public async Task TestParsing()
+        {
+            var response = await File.ReadAllTextAsync(Path.Combine("data", "20191020bnr.txt"));
+            var m = new MockHttpMessageHandler();
+            m.When("https://www.bnr.ro/nbrfxrates.xml")
+                .Respond("application/text", response);
+
+            var nbr = new GetNBRExchange(m);
+            await foreach (var e in nbr.GetActualRates())
+            {
+                if (e.ExchangeFrom == "EUR")
+                {
+                    Assert.Equal(4.7572m, e.ExchangeValue,4);
                     return;
                 }
             }

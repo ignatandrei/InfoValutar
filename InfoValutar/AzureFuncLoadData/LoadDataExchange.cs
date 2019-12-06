e13ddbf71ed60480ca519cfd1f5349bd026cbf1c
Andrei Ignat <ignatandrei@yahoo.com>
2019-12-06 07:32:02 +0200

    repair CI

--- InfoValutar/AzureFuncLoadData/LoadDataExchange.cs
+++ InfoValutar/AzureFuncLoadData/LoadDataExchange.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using InfoValutarLoadingLibs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -10,8 +12,18 @@ namespace AzureFuncLoadData
         [FunctionName("AllData")]
         public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now} next {myTimer.FormatNextOccurrences(1)} ");
-            log.LogInformation("Done");
+            try
+            {
+                log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now} next {myTimer.FormatNextOccurrences(1)} ");
+                var loader = new LoadExchangeProviders("plugins");
+                var exchange = loader.LoadExchange().ToArray();
+                log.LogInformation($"plugin number: {exchange.Length}");
+            }
+            catch(Exception ex)
+            {
+                log.LogError("ERROR! " + ex.Message);
+                throw;
+            }
         }
     }
 }

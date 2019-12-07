a5e74c66703679085787a91ce242b28c42b69f58
ignatandrei <ignatandrei@yahoo.com>
2019-12-07 09:12:34 +0200

    plugins and ci

--- InfoValutar/AzureFuncLoadData/LoadDataExchange.cs
+++ InfoValutar/AzureFuncLoadData/LoadDataExchange.cs
@@ -15,16 +15,20 @@ namespace AzureFuncLoadData
         {
             try
             {
-                log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now} next {myTimer.FormatNextOccurrences(1)} ");
+                log.LogInformation($"%%% OK trigger function executed at: {DateTime.Now} next {myTimer.FormatNextOccurrences(1)} ");
                 var folder = Path.Combine(context.FunctionDirectory, "plugins");
-                log.LogInformation($"Folder {folder} Folder exists: {Directory.Exists(folder)}");
-                //var loader = new LoadExchangeProviders("plugins");
+                log.LogInformation($"%%%  Folder {folder} Folder exists: {Directory.Exists(folder)}");
+                
+                folder = Path.Combine(context.FunctionAppDirectory, "plugins");
+                log.LogInformation($"%%% Folder {folder} Folder exists: {Directory.Exists(folder)}");
+                //var loader = new LoadExchangeProviders(folder);
                 //var exchange = loader.LoadExchange().ToArray();
-                //log.LogInformation($"plugin number: {exchange.Length}");
+                //log.LogInformation($"%%% plugin number: {exchange.Length}");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                log.LogError("ERROR! " + ex.Message);
+                log.LogError("ERROR! " + ex.Message );
+                log.LogError("!!!" + ex.StackTrace);
                 throw;
             }
         }

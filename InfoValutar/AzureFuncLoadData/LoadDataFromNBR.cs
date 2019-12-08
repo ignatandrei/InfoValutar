ab60a119ec786bddab8ab88248b4cfc419b23928
ignatandrei <ignatandrei@yahoo.com>
2019-12-08 14:02:17 +0200

    saving data

--- InfoValutar/AzureFuncLoadData/LoadDataFromNBR.cs
+++ InfoValutar/AzureFuncLoadData/LoadDataFromNBR.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
+using InfovalutarDB;
 using InfoValutarNBR;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,17 +12,28 @@ namespace AzureFuncLoadData
     public static class LoadDataFromNBR
     {
         [FunctionName("LoadDataFromNBR")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
             
             log.Info($"LOAD NBR function processed a request: {DateTime.Now}");
             Console.WriteLine("------------------------!!");
             var nbr = new GetNBRExchange();
-            var data = nbr.GetActualRates().GetAwaiter().GetResult();
+            var data = await nbr.GetActualRates();
             foreach (var item in data)
             {
                 log.Info($"1 {item.ExchangeFrom} = {item.ExchangeValue} {item.ExchangeTo}");
 
+            }
+            log.Info("now saving to database");
+            try
+            {
+                log.Info("trying to save");
+                ISave save = new SaveSqlServer(null);
+                await save.Save(data.ToArray());
+            }
+            catch(Exception ex)
+            {
+                log.Error($"ERROR !! {ex.Message}");
             }
         }
     }

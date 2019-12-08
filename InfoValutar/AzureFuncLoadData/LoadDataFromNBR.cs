538bf06cd5c4c9c19dd965da0a82eba652b8f1b4
ignatandrei <ignatandrei@yahoo.com>
2019-12-08 10:37:20 +0200

    tracewriter

--- InfoValutar/AzureFuncLoadData/LoadDataFromNBR.cs
+++ InfoValutar/AzureFuncLoadData/LoadDataFromNBR.cs
@@ -9,16 +9,16 @@ namespace AzureFuncLoadData
     public static class LoadDataFromNBR
     {
         [FunctionName("LoadDataFromNBR")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
             
-            log.LogInformation($"LOAD NBR function processed a request: {DateTime.Now}");
+            log.Info($"LOAD NBR function processed a request: {DateTime.Now}");
             
             var nbr = new GetNBRExchange();
             var data = nbr.GetActualRates().GetAwaiter().GetResult();
             foreach (var item in data)
             {
-                log.LogInformation($"1 {item.ExchangeFrom} = {item.ExchangeValue} {item.ExchangeTo}");
+                log.Info($"1 {item.ExchangeFrom} = {item.ExchangeValue} {item.ExchangeTo}");
 
             }
         }

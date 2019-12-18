750c45d0faa37cd1e7fce29c9a5afd20de508663
Andrei Ignat <ignatandrei@yahoo.com>
2019-12-18 06:02:18 +0200

    info about last commit

---
+++ InfoValutar/InfoValutarWebAPI/Controllers/InfoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoValutarWebAPI.Controllers
+{
+    /// <summary>
+    /// info about commit
+    /// </summary>
+    public class LastCommitInfo
+    {
+        /// <summary>
+        /// comment latest commit
+        /// </summary>
+        public string LatestCommit { get; set; }
+        /// <summary>
+        /// last date of commit
+        /// </summary>
+        public DateTime DateCommit { get; set; }
+        /// <summary>
+        /// last author of commit
+        /// </summary>
+        public string LastAuthor { get; set; }
+    }
+    /// <summary>
+    /// controller about info the application
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class InfoController
+    {
+        /// <summary>
+        /// info about latest commit
+        /// </summary>
+        /// <returns></returns>
+        public LastCommitInfo GetLatestCommit()
+        {
+            return
+                new LastCommitInfo()
+                {
+                    LatestCommit = "{LatestCommit}",
+                    DateCommit = DateTime.ParseExact("{DateCommit}", "yyyyMMdd:HHmmss", null),
+                    LastAuthor = "{LastAuthor}"
+                }
+                ;
+        }
+    }
+}

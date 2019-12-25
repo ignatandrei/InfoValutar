5208036a4cb1da719692966880236dc33b1b2e74
ignatandrei <ignatandrei@yahoo.com>
2019-12-25 13:02:46 +0200

    copy angular site

---
+++ InfoValutar/copyAng.ps1
@@ -0,0 +1,13 @@
+echo "starting build angular"
+cd InfovalutarWebAng
+npm i
+ng build  --prod --build-optimizer
+cd ..
+
+$source= "InfovalutarWebAng/dist/InfovalutarWebAng/"
+$dest= "InfoValutarWebAPI/wwwroot/"
+echo "delete files"
+Get-ChildItem -Path $dest -Include *.* -File -Recurse | foreach { $_.Delete()}
+echo "copy files"
+Get-ChildItem -Path $source | Copy-Item -Destination $dest
+
